0f4d4c4e7cfb3e61b89bbe7b9bfda9f63fa141c5
JoseOliden <158219397+JoseOliden@users.noreply.github.com>
2025-07-07 11:35:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,7 +103,7 @@ if iniciar:
     grafico = st.empty()
     espectro1 = simular_espectro(0)
     for t_min in range(0, t_max + 1, paso_tiempo):
-        espectro = simular_espectro(t_min)+espectro1
+        espectro += espectro1
         espectro1 = simular_espectro(t_min)
         
         fig, ax = plt.subplots(figsize=(10, 4))

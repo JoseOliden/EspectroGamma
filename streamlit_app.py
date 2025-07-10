3ace48b5a04bbf98502c9ea8653c2a756499b777
JoseOliden <158219397+JoseOliden@users.noreply.github.com>
2025-07-10 11:26:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,7 +80,7 @@ def simular_espectro(t_actual, sel):
         t12 = datos['t12_min']
         gammas = datos['gammas']
         if nuc == "56Mn":
-            f_a = 0.02
+            f_a = 0.05
         if nuc == "28Al":
             f_a = 100
         
